1c8c7078d6e0265200801d1bbc31079e4e28f3ed
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-02-03 18:57:08 +0300

    .

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / 'html'
 
-file_contact_html = DATA_DIR / 'contact.html'
+file_contact_html = DATA_DIR / 'Сontact.html'
